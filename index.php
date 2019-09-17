bc5f38557f790d2b8c196715106045eada67e08f
Rachmann <rachmann.joubert@gmail.com>
2019-09-17 15:51:58 +0100

    Removed inline code in dropdown and uncommented necessary code

--- index.php
+++ index.php
@@ -4,69 +4,74 @@ require_once 'utilities/DB.php';
 $dbConnection = new DB();
 
 $db = $dbConnection->getPDO();
-
 $breeds = [];
-
 $breedSql = $db->prepare('SELECT `breed` FROM `breed`');
-
 $breedSql->execute();
-
 $breeds = $breedSql->fetchAll();
 
-//function drawCats(array $cats) :string {
-//    $stringyCats = '';
-//    foreach($cats as $cat) {
-//        $stringyCats .= '<div class="cat-image"><img src="' . $cat->image . '" alt="A cat"></div>';
-//    }
-//    return $stringyCats;
-//}
-//
-//$catshtml = drawCats($cats);
+function populateDropdown($breeds)
+{
+    $stringyBreeds = '';
+
+    for ($i = 0; $i < count($breeds); $i++) {
+        $breed = $breeds[$i]['breed'];
+        $stringyBreeds .= "<option>$breed</option>";
+    }
+    return $stringyBreeds;
+}
+
+function drawCats(array $cats): string
+{
+    $stringyCats = '';
+    foreach ($cats as $cat) {
+        $stringyCats .= '<div class="cat-image"><img src="' . $cat->image . '" alt="A cat"></div>';
+    }
+    return $stringyCats;
+}
+
+$dropdownBreeds = populateDropdown($breeds);
+
+$catshtml = drawCats($cats);
 
 var_dump($breeds[0]);
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+<head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-git finitowidth, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Top Cat</title>
     <link rel="stylesheet" href="style.css">
-  </head>
-  <body>
-    <div class="header">
-      <div class="container">
+</head>
+<body>
+<div class="header">
+    <div class="container">
         <h1>Top Cat</h1>
         <form action="get">
-          <div class="form-group">
-            <label>Select your breed:</label>
-            <div class="selector">
-              <select name="breed" id="select-breed">
-                <option value="0">Please select:</option>
-                <?php
-
-                for($i = 0; $i < count($breeds); $i++) {
-                    $breed = $breeds[$i]['breed'];
-                    echo "<option>$breed</option>";
-                }
-
-                ?>
-              </select>
+            <div class="form-group">
+                <label>Select your breed:</label>
+                <div class="selector">
+                    <select name="breed" id="select-breed">
+                        <option value="0">Please select:</option>
+                        <?php
+                        echo $dropdownBreeds
+                        ?>
+                    </select>
+                </div>
             </div>
-          </div>
         </form>
-      </div>
     </div>
-    <div class="container">
-      <div class="cat-pictures">
+</div>
+<div class="container">
+    <div class="cat-pictures">
         <?php
-//            echo $catshtml;
+        echo $catshtml;
         ?>
-      </div>
     </div>
-    <div class="footer">
+</div>
+<div class="footer">
 
-    </div>
-  </body>
+</div>
+</body>
 </html>
