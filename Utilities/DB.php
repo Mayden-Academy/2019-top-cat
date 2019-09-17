5a9d0d48027db846fbef00318ca8f564ee33ab5f
AlisonYoon <politescarlett1@gmail.com>
2019-09-17 10:33:01 +0100

    DB connection method created inside DB class

--- Utilities/DB.php
+++ Utilities/DB.php
@@ -4,4 +4,16 @@
 class DB
 {
 
+    private $serverdb = 'mysql:host=192.168.20.20; db=name=';
+    private $dbname = '';
+    private $username = 'root';
+    private $password = '';
+
+    // Create connection
+    public function dbConnect($serverdb, $dbname, $username, $password) {
+        public $dbconnect = new PDO($serverdb . $dbname, $username, $password);
+        $dbconnect->setAttribute(
+            PDO::ATTR_DEFAULT_FETCH_MODE,
+            PDO::FETCH_ASSOC);
+    }
 }
