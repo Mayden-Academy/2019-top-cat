292bc42b75476e436ad38cdead53e1ea2acdd8a4
AlisonYoon <politescarlett1@gmail.com>
2019-09-17 09:58:39 +0100

    variables created

--- ref/fill-db.php
+++ ref/fill-db.php
@@ -4,3 +4,6 @@ $serverdb = "mysql:host=192.168.20.20; db=name=";
 $dbname = '';
 $username = 'root';
 $password = '';
+
+// Create connection
+$dbconnect = new PDO($serverdb . $dbname, $username, $password);
