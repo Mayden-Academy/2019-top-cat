ce5457fc355250916443fcb4a2c8c55c860b0614
Cat <blackcatstone@gmail.com>
2019-09-17 10:36:51 +0100

    Change boilerplate

--- index.php
+++ index.php
@@ -6,9 +6,8 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>title</title>
-    <meta name="description" content="">
-    <link rel="stylesheet" href="">
+    <title>Top Cat</title>
+    <link rel="stylesheet" href="style.css">
   </head>
   <body>
     
