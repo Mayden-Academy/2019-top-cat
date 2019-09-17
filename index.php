6912f312bff220dda41b5fac163b0dbfeb821f12
Cat <blackcatstone@gmail.com>
2019-09-17 11:09:02 +0100

    Apply first-pass at styling

--- index.php
+++ index.php
@@ -10,18 +10,40 @@
     <link rel="stylesheet" href="style.css">
   </head>
   <body>
-    <div class="container">
-      <h1>Top Cat</h1>
-      <form action="post">
-        <label>Select your breed:</label>
-        <select name="breed" id="select-breed">
-          <option value="">Please select:</option>
+    <div class="header">
+      <div class="container">
+        <h1>Top Cat</h1>
+        <form action="post">
+          <div class="form-group">
+            <label>Select your breed:</label>
+            <select name="breed" id="select-breed">
+              <option value="">Please select:</option>
+              <?php ?>
+            </select>
+          </div>
+        </div>
+      </form>
+      </div>
+      <div class="container">
+        <div class="cat-pictures">
           <?php ?>
-        </select>
-    </form>
-    <div class="cat-pictures">
-      <?php ?>
+          <div class="cat-image">
+            <img src="cat-pic-placeholder.jpg" alt="" srcset="">
+          </div>
+          <div class="cat-image">
+            <img src="cat-pic-placeholder.jpg" alt="" srcset="">
+          </div>
+          <div class="cat-image">
+            <img src="cat-pic-placeholder.jpg" alt="" srcset="">
+          </div>
+          <div class="cat-image">
+            <img src="cat-pic-placeholder.jpg" alt="" srcset="">
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
+    <div class="footer">
+
+    </div>
   </body>
 </html>
