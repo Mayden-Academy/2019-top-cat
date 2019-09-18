492f5f333edbf708d39f41af81ca97ee40fd05b8
Tom Watkinson <tomwatkinson90@gmail.com>
2019-09-18 13:43:33 +0100

    created CatHydrator class

--- Hydrators/CatHydrator.php
+++ Hydrators/CatHydrator.php
@@ -1,1 +1,23 @@
 <?php
+
+namespace hydrators;
+
+class CatHydrator
+{
+    private $db;
+
+    public function __construct(PDO $db)
+    {
+        $this->db = $db;
+    }
+
+    public function createCatEntities(string $breedID): array
+    {
+        $sqlCommand = 'SELECT `id`, `img_src`, `breed_id` FROM `img` WHERE `breed_id` = ' . $breedID;
+        $this->db->prepare($sqlCommand);
+        $this->db->execute();
+
+        $cats = $this->db->fetchAll();
+        return $cats;
+    }
+}
