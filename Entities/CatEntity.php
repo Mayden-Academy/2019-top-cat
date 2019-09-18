d5e76b04204967382a9d4ae57a51f549bc9f8bd5
Tom Watkinson <tomwatkinson90@gmail.com>
2019-09-18 13:25:56 +0100

    created CatEntity class

--- Entities/CatEntity.php
+++ Entities/CatEntity.php
@@ -1,7 +1,32 @@
 <?php
 
+namespace entities;
 
 class CatEntity
 {
+    private $id;
+    private $image;
+    private $breed;
 
+    public function __construct($id, $image, $breed)
+    {
+        $this->id = $id;
+        $this->image = $image;
+        $this->breed = $breed;
+    }
+
+    public function getID()
+    {
+        return $this->id;
+    }
+
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    public function getBreed()
+    {
+        return $this->breed;
+    }
 }
