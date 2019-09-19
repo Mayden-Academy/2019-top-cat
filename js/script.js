d216dc42fb84a6cc67153a2249d1d8dc4fd421e9
Tom Watkinson <tomwatkinson90@gmail.com>
2019-09-19 15:16:12 +0100

    removed redundant commented code

--- js/script.js
+++ js/script.js
@@ -2,21 +2,5 @@ let target = document.querySelectorAll('.favorite-icon')
 
 target.forEach(cat => {
     cat.addEventListener('click', function () {
-
         this.parentElement.submit()
-
-        //     let data = {
-        //         "newFavorite": cat.dataset.catID,
-        //         "breedID": cat.dataset.breedID
-        //     }
-        //     fetch('index.php', {
-        //         method: 'POST',
-        //         body: JSON.stringify(data),
-        //         headers: {
-        //             'Content-Type': 'application/json'
-        //         }
-        //     }).then(function (response) {
-        //         console.log(response)
-        //     })
-        // })
     })})
